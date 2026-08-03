3fc1ca337a528017c3055858f1b687541f7f1c6e
murtadaomn-stack <murtadaomn@gmail.com>
2026-08-03 13:45:06 +0300

    Update database.py

--- database.py
+++ database.py
@@ -1,1 +1,32 @@
-# إدارة قاعدة البيانات (سيتم تطويره لاحقاً)
+import sqlite3
+
+DB_NAME = "books.db"
+
+def init_db():
+    conn = sqlite3.connect(DB_NAME)
+    cur = conn.cursor()
+
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS books(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        file_name TEXT,
+        telegram_file_id TEXT UNIQUE,
+        pages INTEGER,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    )
+    """)
+
+    conn.commit()
+    conn.close()
+
+def add_book(file_name, telegram_file_id, pages):
+    conn = sqlite3.connect(DB_NAME)
+    cur = conn.cursor()
+
+    cur.execute("""
+    INSERT OR IGNORE INTO books(file_name,telegram_file_id,pages)
+    VALUES(?,?,?)
+    """,(file_name,telegram_file_id,pages))
+
+    conn.commit()
+    conn.close()
