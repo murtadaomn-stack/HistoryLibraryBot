2cf4365850acaca63f6ca32ac62cf92f722ae753
murtadaomn-stack <murtadaomn@gmail.com>
2026-08-04 10:32:15 +0300

    Update telegram_client.py

--- managers/telegram_client.py
+++ managers/telegram_client.py
@@ -9,15 +9,15 @@ API_ID = int(os.getenv("API_ID"))
 API_HASH = os.getenv("API_HASH")
 PHONE = os.getenv("PHONE")
 
+# إنشاء مجلد sessions إذا لم يكن موجودًا
+os.makedirs("sessions", exist_ok=True)
+
 client = TelegramClient(
     "sessions/history_library",
     API_ID,
     API_HASH
 )
 
-
 async def connect():
-
     await client.start(phone=PHONE)
-
     return client
